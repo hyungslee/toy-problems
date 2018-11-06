e75d82559a65b46c0555479c129e0c38b18cc55d
hyungslee <filterlee@gmail.com>
2018-11-06 10:04:37 +0900

    이형섭 toy01

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -17,7 +17,34 @@
 *
 */
 
-var rockPaperScissors = function (
-) {
+var rockPaperScissors = function(num) {
   // TODO: your solution here
+  var rps = ["rook", "paper", "scissors"];
+
+  var result = [];
+
+  var collect = [];
+  //   for (var i = 0; i < num; i++) {
+  //     collect.push(rps[i]);
+  //     for (var j = 0; j < num; j++) {
+  //       collect.push(rps[j]);
+  //       for (var k = 0; k < num; k++) {
+  //         collect.push(rps[k]);
+  //       }
+  //     }
+  //   }
+  result.push(collect);
+  //   for (var i = 0; i < num; i++) {
+  //     let maker = [];
+
+  //     for (var j = 0; j < num; j++) {
+  //       for (var k = 0; k < num; k++) {
+  //         maker.push(rPs[i]);
+  //         maker.push(rPs[j]);
+  //         maker.push(rPs[k]);
+  //         result.push(maker);
+  //       }
+  //     }
+  //   }
+  return result;
 };
