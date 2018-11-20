358428a1c1e3b471b927049658a8399ec7c94f0d
hyungslee <filterlee@gmail.com>
2018-11-21 08:49:58 +0900

    re..

--- 01-rockPaperScissors/rockPaperScissors.js
+++ 01-rockPaperScissors/rockPaperScissors.js
@@ -19,18 +19,38 @@
 
 var rockPaperScissors = function() {
   // TODO: your solution here
+  // var rps = ["rook", "paper", "scissors"];
+
+  // var result = [];
+  // var collect = [];
+
+  // for (var i = 0; i < rps.length; i++) {
+  //   for (var j = 0; j < rps.length; j++) {
+  //     for (var k = 0; k < rps.length; k++) {
+  //       collect = [rps[i], rps[j], rps[k]];
+  //       result.push(collect);
+  //     }
+  //   }
+  // }
+  // return result;
+
+  round = round || 3
+  var result = []
+
   var rps = ["rook", "paper", "scissors"];
 
-  var result = [];
-  var collect = [];
-
-  for (var i = 0; i < rps.length; i++) {
-    for (var j = 0; j < rps.length; j++) {
-      for (var k = 0; k < rps.length; k++) {
-        collect = [rps[i], rps[j], rps[k]];
-        result.push(collect);
-      }
+  var combos = function(roundsToGo, playedSoFar) {
+    if( roundsToGo === 0 ){
+      result.push( playedSoFar );
+      return;
     }
-  }
+
+    for( var i = 0; i < plays.length; i++ ){
+      var currentPlay = plays[i];
+      combos( roundsToGo-1, playedSoFar.concat(currentPlay) );
+    }
+  };
+  combos( round, [] );
+
   return result;
 };
