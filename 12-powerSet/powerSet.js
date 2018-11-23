9230d0289655047573400c8de5e0c8a303d0e7e0
hyungslee <filterlee@gmail.com>
2018-11-23 10:06:04 +0900

    IM09 이형섭 toy12

--- 12-powerSet/powerSet.js
+++ 12-powerSet/powerSet.js
@@ -7,15 +7,30 @@
  * powerSet("abc")
  * -> [ '' , 'a', 'b', 'c', 'ab', 'ac', 'bc', 'abc' ]
  *
- * Note: 
+ * Note:
  *  1. All characters in a subset should be sorted.
- *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same. 
- * 
+ *  2. Sets of the same characters are considered duplicates regardless of order and count only once, e.g. 'ab' and 'ba' are the same.
+ *
  * Example 2 :
- * 
+ *
  * powerSet("jump")
  * -> ["", "j", "ju", "jm", "jp", "jmu", "jmp", "jpu", "jmpu", "u", "m", "p", "mu", "mp", "pu", "mpu"]
  */
 
-var powerSet = function(str){
+var powerSet = function (str) {
+  var splitStr = str.split('')
+  var result = ['']
+
+  for (var i = 0; i < splitStr.length; i++) {
+    result.push(splitStr[i])
+  }
+  var len = result.length
+  for (var i = 1; i < len; i++) {
+    for (var j = i + 1; j < len; j++) {
+      result.push(result[i] + result[j])
+    }
+  }
+  console.log(result)
+
+  return result
 }
