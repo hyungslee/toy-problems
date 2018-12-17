cbc26cc1624438f7d564e82c1f1b82b80c80103a
hyungslee <filterlee@gmail.com>
2018-12-17 10:07:54 +0900

    toy21 이형섭

--- 21-fractionConverter/fractionConverter.js
+++ 21-fractionConverter/fractionConverter.js
@@ -1,17 +1,31 @@
 /**
- * Write a function that takes a number as its argument and 
+ * Write a function that takes a number as its argument and
  * returns a string that represents that number's simplified fraction.
  *
  * Example: toFraction(0.5) === '1/2'
- * 
+ *
  * Whole numbers and mixed fractions should be returned as irregular fractions
- * 
+ *
  * Example: toFraction(3.0) === '3/1'
- * 
+ *
  * Example: toFraction(2.5) === '5/2'
  *
  */
 
 var toFraction = function(number) {
-  // Your code here
+  var result = "";
+  var count = 0;
+  var a, b;
+
+  if (!Number.isInteger(number)) {
+    count = count + 1;
+    number = number * 10;
+    toFraction(number);
+  } else {
+    a = number;
+    b = Math.pow(10, count);
+    result = `${a}/${b}`;
+  }
+
+  return result;
 };
