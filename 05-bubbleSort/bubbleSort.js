aff396d625ba3c29d71bec49f4fe434b95c267f2
corine <filterlee@gmail.com>
2019-02-06 17:10:58 +0900

    make check complete

--- 05-bubbleSort/bubbleSort.js
+++ 05-bubbleSort/bubbleSort.js
@@ -37,17 +37,17 @@ var i;
 
 // Feel free to add helper functions if needed.
 
-var bubbleSort = function(array) {
+var bubbleSort = function(arr) {
   // Your code here.
   num = 0;
-  for (var i = 0; i < array.length; i++) {
-    for (var j = 0; j < array.length - (i + 1); j++) {
-      if (array[j] > array[j + 1]) {
-        num = array[j];
-        array[j] = array[j + 1];
-        array[j + 1] = num;
+  for (var i = 0; i < arr.length; i++) {
+    for (var j = 0; j < arr.length - (i + 1); j++) {
+      if (arr[j] > arr[j + 1]) {
+        num = arr[j];
+        arr[j] = arr[j + 1];
+        arr[j + 1] = num;
       }
     }
   }
-  return array;
+  return arr;
 };
