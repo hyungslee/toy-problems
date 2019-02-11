7ee3ea96c94610e63b0229fab6d3651b3f017048
corine <filterlee@gmail.com>
2019-02-11 15:14:17 +0900

    테스트가 안맞는것 같습니다.

--- 10-htmljQueryColor/main.js
+++ 10-htmljQueryColor/main.js
@@ -1,4 +1,4 @@
-$(function () {
+$(function() {
   // --------------STEP 1--------------
   // wrap every word in every `<p>` tag with a `<span>` tag.
   // for example: <p>Hey there</p>
@@ -12,28 +12,34 @@ $(function () {
 
   // TODO: your code here!
 
-  var a = $('p').text().split(' ')
-  // console.log(b)
+  var textArr =
+    "Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
 
-  for (var i = 6; i < a.length / 2 - 4; i++) {
-    var pTag = $('p').append('<span>' + a[i] + ' ' + '<span/>')
+  var a = textArr.split(" ");
+
+  for (var j = 0; j < 1; j++) {
+    $("div").append("<p></p>");
+    $("div").append("<p></p>");
+    for (var i = 0; i < a.length; i++) {
+      $("p").append("<span>" + a[i] + " " + "<span/>");
+    }
   }
-})
+});
 
-function changergb () {
-  var text = document.querySelectorAll('span')
+function changergb() {
+  var text = document.querySelectorAll("span");
 
   for (var i = 0; i < text.length; i++) {
-    var r = Math.round(Math.random() * 255)
-    var g = Math.round(Math.random() * 255)
-    var b = Math.round(Math.random() * 255)
+    var r = Math.round(Math.random() * 255);
+    var g = Math.round(Math.random() * 255);
+    var b = Math.round(Math.random() * 255);
 
-    var random_color = 'rgb(' + r + ',' + g + ',' + b + ')'
+    var random_color = "rgb(" + r + "," + g + "," + b + ")";
 
-    text[i].style.color = random_color
+    text[i].style.color = random_color;
   }
 }
 
-setInterval(function () {
-  changergb()
-}, 1000)
+setInterval(function() {
+  changergb();
+}, 1000);
