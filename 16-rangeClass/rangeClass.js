274407387823253e54973ecd299b6beb0746a848
corine <filterlee@gmail.com>
2019-02-19 15:29:05 +0900

    toy16 complete

--- 16-rangeClass/rangeClass.js
+++ 16-rangeClass/rangeClass.js
@@ -38,18 +38,58 @@
  * evenNumbers.includes(2) should be true, evenNumbers.includes(3) should be false
  */
 
-
 var Range = function(start, end, step) {
+  if (start === undefined) {
+    return null;
+  } else {
+    this.start = start;
+  }
+
+  if (end === undefined) {
+    this.end = this.start;
+  } else {
+    this.end = end;
+  }
+
+  if (step === undefined) {
+    this.step = this.start < this.end ? 1 : -1;
+  } else {
+    this.step = step;
+  }
+
+  return this;
 };
 
-Range.prototype.size = function () {
+Range.prototype.size = function() {
+  return Math.floor((this.end - this.start) / this.step + 1);
 };
 
-Range.prototype.each = function (callback) {
+Range.prototype.each = function(callback) {
+  if (this.start < this.end) {
+    for (var i = this.start; i <= this.end; i += this.step) {
+      callback(i);
+    }
+  } else {
+    for (i = this.start; i >= this.end; i += this.step) {
+      callback(i);
+    }
+  }
 };
 
-Range.prototype.includes = function (val) {
+Range.prototype.includes = function(val) {
+  if (this.start < this.end) {
+    return (
+      val >= this.start &&
+      val <= this.end &&
+      (this.start - val) % this.step === 0
+    );
+  } else {
+    return (
+      val <= this.start &&
+      val >= this.end &&
+      (this.start - val) % this.step === 0
+    );
+  }
 };
 
 var range = new Range(1);
-
