4d67888e60ea46d6e0248b36123d09f64145c774
corine <filterlee@gmail.com>
2019-02-25 15:35:05 +0900

    toy20 complete

--- 20-mergeSort/mergeSort.js
+++ 20-mergeSort/mergeSort.js
@@ -95,8 +95,34 @@
  *
  */
 
+var mergeSort = function(arr) {
+  if (arr.length < 2) return arr;
 
+  const mid = Math.floor(arr.length / 2);
 
-var mergeSort = function(array) {
-  // Your code here.
+  const leftArray = arr.slice(0, mid);
+  const rightArray = arr.slice(mid);
+
+  const merge = (leftArray, rightArray) => {
+    const mergedArray = [];
+
+    let leftIndex = 0;
+    let rightIndex = 0;
+
+    while (leftIndex < leftArray.length && rightIndex < rightArray.length) {
+      if (leftArray[leftIndex] < rightArray[rightIndex]) {
+        mergedArray.push(leftArray[leftIndex]);
+        leftIndex++;
+        continue;
+      }
+
+      mergedArray.push(rightArray[rightIndex]);
+      rightIndex++;
+    }
+
+    return mergedArray
+      .concat(leftArray.slice(leftIndex))
+      .concat(rightArray.slice(rightIndex));
+  };
+  return merge(mergeSort(leftArray), mergeSort(rightArray));
 };
